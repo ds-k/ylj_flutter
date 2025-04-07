1882d3bc4bfb06004484f50e2f7ea7a0f7f9e7ed
Daeseong Kim <daeseongkim05@gmail.com>
2025-04-07 14:29:57 +0900

    chore: providerscope 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ylj_flutter/presentation/ui/layout/layout_page.dart';
 import 'constant/colors.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
